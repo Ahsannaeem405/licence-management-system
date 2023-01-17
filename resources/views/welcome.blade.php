8fa0b239d2f16a1f8c51c1204fa2719df8777c6b
Ali Raza <alihaider4938495@gmail.com>
2023-01-17 18:41:07 +0500

    accordian

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -511,7 +511,9 @@
                         </button>
                     </h2>
                     <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+                        <div class="accordion-body">
+                        which is intended to demonstrate the class content for this accordion, which is intended to demonstrate the class content for this accordion, which is intended to demonstrate the class content for this accordion, which is intended to demonstrate the class content for this accordion, which is intended to demonstrate the class. This is the first item's accordion body.
+                        </div>
                     </div>
                 </div>
                 <div class="accordion-item">
@@ -521,7 +523,7 @@
                         </button>
                     </h2>
                     <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                        <div class="accordion-body">which is intended to demonstrate the class content for this accordion, which is intended to demonstrate the class content for this accordion, which is intended to demonstrate the class content for this accordion, which is intended to demonstrate the class content for this accordion, which is intended to demonstrate the class. This is the first item's accordion body.</div>
                     </div>
                 </div>
             </div>

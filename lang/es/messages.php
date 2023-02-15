4a736ff1dc998ab51ee5d30a5cdd7a89b73823b1
wali926 <waleedmahmood926@gmail.com>
2023-02-15 16:36:35 +0500

    updated code

--- lang/es/messages.php
+++ lang/es/messages.php
@@ -57,5 +57,57 @@ return [
     'total license'=>'Licencia Total',
     'welcome'=>'Bienvenido',
     
+    //------------------------- Welcome Navbar Part -----------------------//
+    'nav-works' => 'How it work',
+    'nav-pricing' => 'Precios',
+    'nav-benifits' => 'Beneficios',
+    'nav-customers' => 'Clientes',
+    'nav-about' => 'Acerca de',
+    'nav-contact' => 'Contacto Us',
+    'nav-login' => 'Login',
+    //------------------------- Welcome Navbar Part -----------------------//
     
+    //------------------------- Welcome Banner Part -----------------------//
+    'banner-heading' => 'Simplifique su gestión de licencias, amplifique su eficiencia',
+    'banner-paragraph' => 'Diga adiós al seguimiento manual de licencias y dé la bienvenida al éxito simplificado con nuestra aplicación de gestión de licencias. Nuestra interfaz fácil de usar y nuestras potentes funciones facilitan la administración y el seguimiento de todas sus licencias en una ubicación central, lo que le permite ahorrar tiempo y aumentar la eficiencia. Regístrese ahora y aproveche nuestra prueba gratuita de 3 meses para experimentar los beneficios por sí mismo.',
+    'banner-button' => 'Reserve una demostración',
+    //------------------------- Welcome Banner Part -----------------------//
+
+    //------------------------- Welcome How it work Part -----------------------//
+    'work-heading' => 'Como funciona',
+    'work-sub-heading' => 'Comience con 3 sencillos pasos',
+    'work-tab-1' => 'Elija su suscripción',
+    'work-tab-1-text' => 'Encuentra el plan perfecto para tu negocio: Compara nuestras opciones de suscripción',
+    'work-tab-2' => 'Crea tu cuenta',
+    'work-tab-2-text' => 'Configure fácilmente su cuenta en minutos: regístrese ahora',
+    'work-tab-3' => 'Administre sus licencias',
+    'work-tab-3-text' => 'Optimice la gestión de licencias: organice y supervise el uso con facilidad',
+    //------------------------- Welcome How it work Part -----------------------//
+
+    //------------------------- Welcome FAQ Part -----------------------//
+    'faq-q-1' => 'Cuál es su aplicación de gestión de licencias?',
+    'faq-a-1' => 'Nuestra aplicación de administración de licencias es un software que ayuda a las empresas y organizaciones a administrar, rastrear y renovar todas sus licencias en una ubicación conveniente y segura. Nuestra interfaz fácil de usar y nuestras potentes funciones permiten a los usuarios estar al tanto de sus necesidades de licencias, para que puedan concentrarse en administrar su negocio.',
+    'faq-q-2' => 'Cómo funciona tu aplicación?',
+    'faq-a-2' => 'Nuestra aplicación permite a los usuarios cargar y almacenar todas sus licencias en una ubicación central. Los usuarios pueden establecer recordatorios para las fechas de renovación, realizar un seguimiento del estado de sus licencias y renovarlas fácilmente cuando sea necesario. La aplicación también genera informes para ayudar a los usuarios a cumplir con los requisitos de licencia.',
+    'faq-q-3' => 'Cómo puede su aplicación ayudar a mi negocio?',
+    'faq-a-3' => 'Nuestra aplicación puede ayudar a su negocio al agilizar y simplificar el proceso de administración de licencias. Puede ahorrarle tiempo y recursos, y garantizar que cumpla con todos los requisitos de licencia. Además, puede ayudarlo a evitar sanciones y multas asociadas con licencias vencidas o no conformes.',
+    'faq-q-4' => 'Puedo usar su solicitud para licencias de varios estados o países?',
+    'faq-a-4' => 'Sí, nuestra aplicación se puede utilizar para gestionar licencias de varios estados o países. Permite a los usuarios rastrear y renovar licencias desde diferentes ubicaciones en un lugar central.',
+    'faq-q-5' => 'Qué tan segura es su aplicación?',
+    'faq-a-5' => 'Nos tomamos la seguridad muy en serio y hemos implementado medidas estrictas para proteger los datos almacenados en nuestra aplicación. Todos los datos están encriptados y almacenados en servidores seguros. Además, cumplimos con todos los protocolos de seguridad estándar de la industria para garantizar que nuestra aplicación sea lo más segura posible.',
+    'faq-q-6' => 'Ofrecen atención al cliente?',
+    'faq-a-6' => 'Sí, ofrecemos atención al cliente a nuestros usuarios. Nuestro equipo de atención al cliente está disponible para ayudarlo con cualquier pregunta o problema que pueda tener. Pueden ayudarlo a comenzar con la aplicación, solucionar cualquier problema que pueda encontrar y brindarle orientación sobre cómo usar las funciones de la aplicación.',
+    'faq-q-7' => 'Puedo probar su aplicación antes de comprarla?',
+    'faq-a-7' => 'Sí, ofrecemos una prueba gratuita de nuestra aplicación de administración de licencias. Durante la prueba, tendrá acceso a todas las funciones de la aplicación y podrá usarla para administrar sus licencias. Después del período de prueba, puede decidir si desea comprar la aplicación.',
+    'faq-q-8' => 'Hay alguna tarifa de instalación?',
+    'faq-a-8' => 'No, no hay tarifa de configuración para usar nuestra aplicación de administración de licencias. Puede comenzar a usarlo inmediatamente después de registrarse.',
+    //------------------------- Welcome FAQ Part -----------------------//
+
+    //------------------------- Welcome About us Part -----------------------//
+    'about-heading' => 'Sobre nosotras',
+    'about-sub-heading' => 'Experimente los beneficios de nuestra aplicación de gestión de licencias a través de las palabras de clientes satisfechos.',
+    'about-p-1' => '¡Bienvenido a nuestra aplicación de gestión de licencias! Somos un equipo dedicado de profesionales apasionados por ayudar a las empresas y organizaciones a optimizar y simplificar sus procesos de gestión de licencias.',
+    'about-p-2' => 'Nuestra aplicación está diseñada para facilitar a los usuarios la administración, el seguimiento y la renovación de todas sus licencias en una ubicación conveniente y segura. Nuestra interfaz fácil de usar y nuestras potentes funciones permiten a los usuarios estar al tanto de sus necesidades de licencias, para que puedan concentrarse en administrar su negocio.',
+    'about-p-3' => 'Entendemos que la gestión de licencias puede ser una tarea compleja y que requiere mucho tiempo, por lo que hemos desarrollado nuestra aplicación para que sea lo más eficiente y eficaz posible. Trabajamos constantemente para mejorar y actualizar nuestra aplicación para garantizar que satisfaga las necesidades cambiantes de nuestros clientes.',
+    //------------------------- Welcome About us Part -----------------------//
 ];
